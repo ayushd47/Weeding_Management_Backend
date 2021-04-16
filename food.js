1d607a028fc63fb2deee1399d700d2af24afa116
Ayush Dangol <ayushdangol47@gmail.com>
2021-04-16 15:12:11 +0545

    first commit

---
+++ food.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const cors = require('cors');
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+const db = require('./database/data')
+const consumer_route = require('./route/Consumer_route')
+const product_route = require('./route/ProductRoute')
+const adimn_route = require('./route/AdminRoute');
+const cartroute=require('./route/CartRoute')
+const profileroute = require('./route/ProfileRoute')
+// const publicDir= path.join(__dirname,'public');
+
+
+app.use(express.static("images"))
+app.use(consumer_route)
+app.use(product_route)
+app.use(adimn_route)
+app.use(cartroute)
+app.use(profileroute)
+
+app.listen(90)
---
+++ route/CartRoute.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const router = express.Router()
+const Cart = require('../model/CartModel')
+const auth = require('../middleware/auth')
+const upload = require('../middleware/upload')
+
+//inserting our product
+router.post('/cart/add',auth.verifyConsumer, function(req, res){
+  
+    console.log(res.ConsumerData.username)
+  const ProductName = req.body.ProductName
+  const ProductPrice = req.body.ProductPrice
+  const ProductDesc = req.body.ProductDesc
+  const ProductAvailable = req.body.ProductAvailable
+  const username =res.ConsumerData.username
+  //console.log(username)
+  
+  //{variable:modelname}
+  const CartData = new Cart({ProductName:ProductName,ProductPrice:ProductPrice,  ProductAvailable:ProductAvailable,ProductDesc:ProductDesc,username:username})
+  CartData.save()
+  .then(function(result){
+      res.status(201).json({success:true})
+  })
+  .catch(function(e){
+      res.status(500).json({message:e})
+  })
+}) 
+
+//get data
+router.get('/cart/all', function(req,res){
+    Cart.find()
+    .then(function(CartData){
+        res.status(200).json({success:true, data:CartData})
+    })
+    .catch(function(e){
+        res.status(500).json({message:e})
+    })
+})
+
+router.get('/cart/:username', function(req, res){
+    const username= req.params.username
+    Cart.find({username:username})
+    .then(function(data){
+        res.status(200).json({
+            success:true, 
+             data:data
+        })
+    })
+    
+})
+
+
+
+module.exports = router
